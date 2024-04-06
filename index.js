dfa4811ce5ac75f00264eeb26064974db32e1dae
Syed Wasif Ali <wa8sifali@gmail.com>
2024-04-06 13:45:21 -0700

    Push project on github

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Choose the operation",
+        type: "list",
+        name: "operation",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Modulus",
+            "Exponentiation",
+        ],
+    },
+]);
+if (answer.operation === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operation === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operation === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operation === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operation === "Modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operation === "Exponentiation") {
+    console.log(answer.firstNumber * answer.firstNumber);
+}
+else {
+    console.log("INVALID OPERATION");
+}
