78203bec66bb734024b23c20b47199955b21f403
Syed Wasif Ali <wa8sifali@gmail.com>
2024-04-07 17:19:17 -0700

    Push calculator on github

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-const answer = await inquirer.prompt([
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
     { message: "Enter first number", type: "number", name: "firstNumber" },
     { message: "Enter second number", type: "number", name: "secondNumber" },
     {
--- index.ts
+++ index.ts
@@ -1,43 +1,40 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
-
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
-  {
-    message: "Choose the operation",
-    type: "list",
-    name: "operation",
-    choices: [
-      "Addition",
-      "Subtraction",
-      "Multiplication",
-      "Division",
-      "Modulus",
-      "Exponentiation",
-    ],
-  },
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Choose the operation",
+        type: "list",
+        name: "operation",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Modulus",
+            "Exponentiation",
+        ],
+    },
 ]);
-
 if (answer.operation === "Addition") {
-  console.log(answer.firstNumber + answer.secondNumber);
+    console.log(answer.firstNumber + answer.secondNumber);
 }
- else if (answer.operation === "Subtraction") {
-  console.log(answer.firstNumber - answer.secondNumber);
+else if (answer.operation === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
 }
- else if (answer.operation === "Multiplication") {
-  console.log(answer.firstNumber * answer.secondNumber);
+else if (answer.operation === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
 }
- else if (answer.operation === "Division") {
-  console.log(answer.firstNumber / answer.secondNumber);
+else if (answer.operation === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
 }
- else if (answer.operation === "Modulus") {
-  console.log(answer.firstNumber % answer.secondNumber);
+else if (answer.operation === "Modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
 }
- else if (answer.operation === "Exponentiation") {
-  console.log(answer.firstNumber * answer.firstNumber);
+else if (answer.operation === "Exponentiation") {
+    console.log(answer.firstNumber * answer.firstNumber);
 }
- else {
-  console.log("INVALID OPERATION");
- }
+else {
+    console.log("INVALID OPERATION");
+}
